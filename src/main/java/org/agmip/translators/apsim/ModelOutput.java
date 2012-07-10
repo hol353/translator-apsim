23673791647bfec46d1ac7a26d6b4f2ad399eba8
Dean Holzworth <Dean.Holzworth@csiro.au>
2012-07-10 11:01:16 -0400

    Moved to using MapUtil.getBucket

--- src/main/java/org/agmip/translators/apsim/ModelOutput.java
+++ src/main/java/org/agmip/translators/apsim/ModelOutput.java
@@ -4,10 +4,13 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.agmip.util.MapUtil.BucketEntry;
 //import java.io.XML;
 import org.agmip.core.types.TranslatorOutput;
 import org.agmip.util.JSONAdapter;
@@ -26,17 +29,18 @@ public class ModelOutput implements TranslatorOutput {
         Velocity.init();
 
         VelocityContext context = new VelocityContext();
-        List weather = MapUtil.getValueOr(input, "weather", new ArrayList()).get(0);
-        context.put( "WeatherFileName", MapUtil.getValueOr(weather, "wst_insi", "?"));
-        
+        BucketEntry weather = MapUtil.getBucket(input, "weather").get(0);
+        context.put( "wst_insi", MapUtil.getValueOr(weather.getValues(), "wst_insi", "?"));
         
         Template template = Velocity.getTemplate("src\\main\\resources\\AgMIPTemplate.apsim");
 
         FileWriter F;
         try {
+            
             F = new FileWriter("Test.apsim");
             template.merge( context, F );
             F.close();
+            
         } catch (IOException ex) {
             Logger.getLogger(ModelOutput.class.getName()).log(Level.SEVERE, null, ex);
         }
