5999f1820729b0231c250b53b25abf438bb0b4c7
Dean Holzworth <Dean.Holzworth@csiro.au>
2013-02-09 01:55:29 +1000

    Added simpler test for reading in APSIM weather files

--- src/test/java/org/agmip/translators/apsim/SoilReaderTest.java
+++ src/test/java/org/agmip/translators/apsim/SoilReaderTest.java
@@ -17,11 +17,11 @@ public class SoilReaderTest {
 
 	@Test
 	public void testReader() throws Exception{
-		SoilReader	sr = new SoilReader("src/test/resources/apsim/SmallWheatValidation.apsim.xml");
+		SoilReader	sr = new SoilReader("src/test/resources/apsim/SmallWheatValidation.apsim");
 		List<Soil> soils = sr.read();
 		assertEquals(2,soils.size());
 		assertEquals("Yucheng",soils.get(0).getID());
-		assertEquals(20,soils.get(1).getLayers().length);
+		assertEquals(10,soils.get(1).getLayers().length);
 	}
 	
 	@Test
--- src/test/java/org/agmip/translators/apsim/WeatherReaderTest.java
+++ src/test/java/org/agmip/translators/apsim/WeatherReaderTest.java
@@ -11,13 +11,13 @@ public class WeatherReaderTest {
 	@Test
 	public void testReader() throws Exception{
 		WeatherReader	weatherReader = new WeatherReader();
-		Weather weather = weatherReader.read("src/test/resources/apsim/APS14.met");
-		assertEquals("-27.31", weather.getLatitude());
-		assertEquals("APS14",weather.getName());
-		assertEquals(178, weather.getRecords().size());
-		assertEquals("19930705", weather.getRecords().get(1).getDate());
-		assertEquals("10.1", weather.getRecords().get(1).getMinTemperature());
-		assertEquals("20.5", weather.getRecords().get(1).getMaxTemperature());
+		Weather weather = weatherReader.read("src/test/resources/apsim/Yucheng.met");
+		assertEquals("36.68", weather.getLatitude());
+		assertEquals("Yucheng",weather.getName());
+		assertEquals(1189, weather.getRecords().size());
+		assertEquals("20021102", weather.getRecords().get(1).getDate());
+		assertEquals("14.7", weather.getRecords().get(1).getMinTemperature());
+		assertEquals("23.1", weather.getRecords().get(1).getMaxTemperature());
 		
 	}
 
